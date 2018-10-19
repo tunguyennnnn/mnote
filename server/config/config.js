c207fc72c8da18ea7ed298639b4b161d4ea84c18
tunguyennnnn <conghoa522@gmail.com>
2018-10-19 00:21:53 -0400

    fix docker compose

---
+++ server/config/config.js
@@ -0,0 +1,23 @@
+export default {
+  'development': {
+    'username': 'postgres',
+    'password': 'postgres',
+    'database': 'v_database',
+    'host': process.env.DB_HOST || '127.0.0.1',
+    'dialect': 'postgres'
+  },
+  'test': {
+    'username': 'root',
+    'password': null,
+    'database': 'database_test',
+    'host': '127.0.0.1',
+    'dialect': 'mysql'
+  },
+  'production': {
+    'username': 'root',
+    'password': null,
+    'database': 'database_production',
+    'host': '127.0.0.1',
+    'dialect': 'mysql'
+  }
+}
